fc9f14557ef55703c54e3a09175d62ef9db38f6a
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2024-08-11 14:19:11 +0200

    chore: fix licenses page on macos

--- lib/features/settings/ui/widgets/about_this_app_button.dart
+++ lib/features/settings/ui/widgets/about_this_app_button.dart
@@ -3,58 +3,82 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../core/core.dart';
+import '../../../features.dart';
 
 class AboutThisAppButton extends ConsumerWidget {
   const AboutThisAppButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return TextButton(
-      onPressed: () {
-        final analyticsService = ref.read(analyticsServiceProvider);
-        analyticsService.emitEvent('about_tapped', {});
-        showAboutDialog(
-          context: context,
-          applicationIcon: Image.asset(
-            'assets/app_icon.png',
-            width: 75,
-            height: 75,
-          ),
-          children: [
-            ListTile(
-              onTap: () {
-                final analyticsService = ref.read(analyticsServiceProvider);
-                analyticsService.emitEvent('terms_and_conditions_tapped', {});
-                const linkString = AppConstants.termsAndConditionsUrl;
-                final link = Uri.parse(linkString);
-                launchUrl(link);
-              },
-              title: const Text('Terms and Conditions'),
+    return Theme(
+      data: CustomTheme.darkTheme().copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              surface: AppColors.backgroundColor,
             ),
-            ListTile(
-              onTap: () {
-                final analyticsService = ref.read(analyticsServiceProvider);
-                analyticsService.emitEvent('privacy_policy_tapped', {});
-                const linkString = AppConstants.privacyPolicyUrl;
-                final link = Uri.parse(linkString);
-                launchUrl(link);
-              },
-              title: const Text('Privacy Policy'),
-            ),
-            ListTile(
-              onTap: () {
-                final analyticsService = ref.read(analyticsServiceProvider);
-                analyticsService.emitEvent('source_code_tapped', {});
-                const linkString = AppConstants.sourceCodeUrl;
-                final link = Uri.parse(linkString);
-                launchUrl(link);
-              },
-              title: const Text('Source code'),
-            ),
-          ],
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.backgroundColor,
+        ),
+        dialogBackgroundColor: AppColors.backgroundColor,
+        canvasColor: AppColors.backgroundColor,
+        cardColor: AppColors.backgroundColor,
+        cardTheme: const CardTheme(
+          color: AppColors.backgroundColor,
+          surfaceTintColor: AppColors.backgroundColor,
+        ),
+      ),
+      child: Builder(builder: (context) {
+        return TextButton(
+          onPressed: () async {
+            final analyticsService = ref.read(analyticsServiceProvider);
+            analyticsService.emitEvent('about_tapped', {});
+
+            showAboutDialog(
+              context: context,
+              useRootNavigator: false,
+              applicationIcon: Image.asset(
+                'assets/app_icon.png',
+                width: 75,
+                height: 75,
+              ),
+              children: [
+                ListTile(
+                  onTap: () {
+                    final analyticsService = ref.read(analyticsServiceProvider);
+                    analyticsService
+                        .emitEvent('terms_and_conditions_tapped', {});
+                    const linkString = AppConstants.termsAndConditionsUrl;
+                    final link = Uri.parse(linkString);
+                    launchUrl(link);
+                  },
+                  title: const Text('Terms and Conditions'),
+                ),
+                ListTile(
+                  onTap: () {
+                    final analyticsService = ref.read(analyticsServiceProvider);
+                    analyticsService.emitEvent('privacy_policy_tapped', {});
+                    const linkString = AppConstants.privacyPolicyUrl;
+                    final link = Uri.parse(linkString);
+                    launchUrl(link);
+                  },
+                  title: const Text('Privacy Policy'),
+                ),
+                ListTile(
+                  onTap: () {
+                    final analyticsService = ref.read(analyticsServiceProvider);
+                    analyticsService.emitEvent('source_code_tapped', {});
+                    const linkString = AppConstants.sourceCodeUrl;
+                    final link = Uri.parse(linkString);
+                    launchUrl(link);
+                  },
+                  title: const Text('Source code'),
+                ),
+              ],
+            );
+          },
+          child: const Text('About this app'),
         );
-      },
-      child: const Text('About this app'),
+      }),
     );
   }
 }
